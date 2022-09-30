5ddae11c016f6d66bc792197b5f91fb6b24c9513
kapv-97 <114553864+kapv-97@users.noreply.github.com>
2022-09-30 15:21:48 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+var addWords = function() {
+    secretMessage.push('to', 'Program');
+}
+addWords();
+
+secretMessage.splice(7, 1, 'right');
+
+secretMessage.splice(0, 1, 'Programming');
+
+secretMessage.splice(6, 5, 'know');
+
+console.log(secretMessage.join());
+
